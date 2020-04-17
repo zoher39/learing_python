6cb7bdd855dd6a51643a5d0751d2224169fc77d2
zoher39 <zohir39@gmail.com>
2020-04-17 12:19:14 +0100

    First Course For Python

---
+++ Data_Types.py
@@ -0,0 +1,20 @@
+ # type()
+ # All Data in Python is an Object
+
+
+print(type(10))    #int => Integer
+print(type(-10))
+
+print(type(10.5))   # float => Floating Point Number
+print(type(10.532))
+print(type(-10.5234))
+
+print(type("Hello Ms SADOUN")) # str => String
+
+print(type([1, 2, 3, 4, 5, 6])) # list => List
+
+print(type((1, 2, 3, 4, 5, 6))) # tuple => Tuple
+
+print(type({"One" : 1, "Tow" : 2, "Three" : 3})) # dict => Dictionary
+
+print(type(2 == 2)) # bool => Boolean valeu : true or false
