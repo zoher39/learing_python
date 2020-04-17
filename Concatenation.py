cdf8eb667a13da795afbff8d5479692f6fe5420c
zoher39 <zohir39@gmail.com>
2020-04-17 12:16:24 +0100

    First Course For Python

---
+++ Concatenation.py
@@ -0,0 +1,24 @@
+#-----------------------------#
+#------- Concatenation -------#
+#-----------------------------#
+
+msg = "I Love"
+lang = "Python"
+
+print(msg + lang)
+print(msg +" "+ lang)
+
+full = msg + " " + lang
+print(full)
+
+a = "Fist \
+Second \
+Third"
+
+b = "A \
+B \
+C"
+
+print(a +" "+b)
+print(a +"\n"+b)
+
